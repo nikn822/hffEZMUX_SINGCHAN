1980c5ca2a7f9c57c688d6be0146b4e30d04d5e2
Nik Nair <nikhiln822@gmail.com>
2022-01-17 12:31:12 -0500

    Update LEDFn.ino

--- examples/LEDFn/LEDFn.ino
+++ examples/LEDFn/LEDFn.ino
@@ -1,10 +1,7 @@
 #include <i2c_t3.h>
 #include <hffEZMUX_SINGCHAN.h>
 
-#define TCA 0x70
-#define TCA2 0x77
-
-TSL2591 test = TSL2591();
+Nanolab test = Nanolab();
 void setup() {
   Serial.begin(9600);
   Serial.println("TESTING");
