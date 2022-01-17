a4820e7e3ede861639c86698a9822d619e163321
Nik Nair <nikhiln822@gmail.com>
2022-01-17 12:30:06 -0500

    Update libraryTest.ino

--- examples/libraryTest/libraryTest.ino
+++ examples/libraryTest/libraryTest.ino
@@ -4,38 +4,15 @@
 #define TCA 0x70
 #define TCA2 0x77
 
-TSL2591 test = TSL2591();
+Nanolab test = Nanolab();
 void setup() {
-  Serial.begin(9600);
-  Serial.println("TESTING");
+  //Serial.begin(9600);
+  //Serial.println("TESTING");
 }
    
 void loop() {
-  test.simpleRead(2,3);
-  Serial.println("TSL Sensor #1");
-  Serial.print(F("[ ")); Serial.print(test.sro.ms); Serial.print(F(" ms ] "));
-  Serial.print(F("Luminosity: "));
-  Serial.println(test.sro.lum, DEC);
 
-  test.simpleRead(2,2);
-  Serial.println("TSL Sensor #2");
-  Serial.print(F("[ ")); Serial.print(test.sro.ms); Serial.print(F(" ms ] "));
-  Serial.print(F("Luminosity: "));
-  Serial.println(test.sro.lum, DEC);
-
-  test.simpleRead(4,3);
-  Serial.println("TSL Sensor #3");
-  Serial.print(F("[ ")); Serial.print(test.sro.ms); Serial.print(F(" ms ] "));
-  Serial.print(F("Luminosity: "));
-  Serial.println(test.sro.lum, DEC);
-
-  test.simpleRead(4,2);
-  Serial.println("TSL Sensor #4");
-  Serial.print(F("[ ")); Serial.print(test.sro.ms); Serial.print(F(" ms ] "));
-  Serial.print(F("Luminosity: "));
-  Serial.println(test.sro.lum, DEC);
-  Serial.println();
-
-  delay(500);
+  test.saveSD();
+  delay(50000);
 
 }
